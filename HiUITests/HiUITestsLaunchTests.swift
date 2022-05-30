b3fdd4f7577fa8d3d73eebedb7faf8fe533cc1b2
Hassaan Fayyaz <hassaanfayyaz@gmail.com>
2022-05-30 19:55:28 +0500

    Project setup

---
+++ HiUITests/HiUITestsLaunchTests.swift
@@ -0,0 +1,20 @@
+//
+//  HiUITestsLaunchTests.swift
+//  HiUITests
+//
+//  Created by Hassaan Fayyaz Ahmed on 5/30/22.
+//
+
+import XCTest
+
+class HiUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+}
